2a9edfd0351918acba62235511917e91ca9c110f
ObserverOfTime <chronobserver@disroot.org>
2025-07-18 10:40:26 +0300

    feat: update bindings

--- bindings/python/tests/test_binding.py
+++ bindings/python/tests/test_binding.py
@@ -1,11 +1,12 @@
 from unittest import TestCase
 
-import tree_sitter, tree_sitter_pem
+from tree_sitter import Language, Parser
+import tree_sitter_pem
 
 
 class TestLanguage(TestCase):
     def test_can_load_grammar(self):
         try:
-            tree_sitter.Language(tree_sitter_pem.language())
+            Parser(Language(tree_sitter_pem.language()))
         except Exception:
             self.fail("Error loading PEM grammar")
